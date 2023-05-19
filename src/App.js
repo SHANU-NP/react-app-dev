d9b25bdda95e1aa4fafe98dfa6fa7aee8174e836
shanuNP <shanunp2016@gmail.com>
2023-05-19 21:31:50 +0530

    -- code refactor.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import ErrorBoundry from "./components/errorHandling/ErrorBoundry";
-import Hero from "./components/errorHandling/Hero";
-import WelcomeMessage from "./components/eventBindingPractise/Welcome";
+
 
 function App() {
   return (
     <div className="App">
-    
-    <WelcomeMessage></WelcomeMessage>
+
 
     </div>
   );
