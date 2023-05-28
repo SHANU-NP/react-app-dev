a46be7020db7f4a0596c96860ae9bf0b92825475
shanuNP <shanunp2016@gmail.com>
2023-05-28 21:22:34 +0530

    feat: higher order function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import ClickCounter from "./components/highOrderFunction/ClickCounter";
+import HoverCounter from "./components/highOrderFunction/HoverCounter";
 
 
 function App() {
   return (
     <div className="App">
 
+        <ClickCounter></ClickCounter>
+        <HoverCounter></HoverCounter>
 
     </div>
   );
---
+++ src/components/highOrderFunction/HoverCounter.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import updatedComponent from './withCounter'
+
+class HoverCounter extends Component {
+
+  render() {
+    const {count,incrementCount} = this.props
+
+    return (
+      <div> 
+        <h1 onMouseOver={incrementCount}>Hovered count {count}</h1>
+      </div>
+    )
+  }
+}
+
+export default updatedComponent(HoverCounter)
