105a06ce81214d078f7996d45d23b408713ad75e
shanuNP <shanunp2016@gmail.com>
2023-06-02 21:21:42 +0530

    -- render props pattern for sharing functionality b/w compnts.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import ClickCounter from "./components/highOrderFunction/ClickCounter";
-import HoverCounter from "./components/highOrderFunction/HoverCounter";
-
+import ClickCounter from "./components/renderPropsPattern/ClickCounter";
+import Counter from "./components/renderPropsPattern/Counter";
+import HoverCounter from "./components/renderPropsPattern/HoverCounter";
 
 function App() {
   return (
     <div className="App">
+      <Counter
+        render={(count, incrementCount) => {
+          return (
+            <ClickCounter
+              count={count}
+              incrementCount={incrementCount}
+            ></ClickCounter>
+          );
+        }}
+      />
 
-        <ClickCounter name = "shanu"></ClickCounter>
-        <HoverCounter></HoverCounter>
-
+      <Counter
+        render={(count, incrementCount) => {
+          return (
+            <HoverCounter
+              count={count}
+              incrementCount={incrementCount}
+            ></HoverCounter>
+          );
+        }}
+      ></Counter>
     </div>
   );
 }
---
+++ src/components/renderPropsPattern/HoverCounter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class HoverCounter extends Component {
+
+  render() {
+    const {count,incrementCount} = this.props
+    return (
+     
+      <div>
+        
+        <h1 onMouseOver={incrementCount}>hover {count} times</h1>
+      </div>
+    )
+    
+  }
+}
+
+export default HoverCounter
