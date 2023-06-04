ed11070b455a7b9b056ad63b664b62bb200eba68
shanuNP <shanunp2016@gmail.com>
2023-06-04 21:13:26 +0530

    -- http get request using axios http library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import ComponentC from "./components/context/ComponentC";
 import { UserProvider } from "./components/context/userContext";
+import PostList from "./components/http/PostList";
 
 function App() {
   return (
-    <div className="App">
-      <UserProvider value={{name : "shanu",age : "20"}}>
-        <ComponentC />
-      </UserProvider>
+    <div>
+      <PostList></PostList>
     </div>
   );
 }
