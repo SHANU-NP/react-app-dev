8c5aab6e681bc2ea22e6c283728cce0b07162590
shanuNP <shanunp2016@gmail.com>
2023-06-07 17:01:46 +0530

    -- useEffect in conditionall call

--- src/components/useEffectHook/HookCounterOne.js
+++ src/components/useEffectHook/HookCounterOne.js
@@ -3,18 +3,19 @@ import React, { useState ,useEffect} from 'react';
 function HookCounterOne(){
 
     const [count,setCount] = useState(0)
+    const [name,setName] = useState('')
+
     //execute each time when render with new state
     useEffect(() => {
         document.title = `you clicked ${count} times`
-    })
+        console.log("document title updated")
+    },[count])
 
     return(
 
         <div>
-
-        <button onClick={() => setCount(count + 1)}>count : {count}</button>
-
-
+            <input type='text' value={name} onChange={e => setName(e.target.value)}></input>
+            <button onClick={() => setCount(count + 1)}>count : {count} times</button>
         </div>
     )
 }
