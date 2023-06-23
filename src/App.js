b73f09813aa7011cb13159c9b42f1ff8e7a5ffe8
shanuNP <shanunp2016@gmail.com>
2023-06-23 11:08:40 +0530

    -- useRef hook with focus on input element.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import React from "react";
 import DataFetchingTwo from "./components/fetchingDataWithUserReducer/DataFetchingTwo";
 import Count from "./components/useMemo/Count";
+import FocusInput from "./components/useRefs/FocusInput";
 
 function App() {
   return (
     <div>
-      <Count></Count>
+      <FocusInput></FocusInput>
     </div>
   );
 }
