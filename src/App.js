83c28d68e01a2af5d0b725c66292fa0a2bde9c3b
shanuNP <shanunp2016@gmail.com>
2023-06-23 12:29:26 +0530

    -- useRef application in hook counter.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import DataFetchingTwo from "./components/fetchingDataWithUserReducer/DataFetchi
 import Count from "./components/useMemo/Count";
 import FocusInput from "./components/useRefs/FocusInput";
 import ClassTimer from "./components/useRefs/ClassTimer";
+import HookTimer from "./components/useRefs/HookTimer";
 
 function App() {
   return (
     <div>
       <ClassTimer/>
+      <HookTimer/>
     </div>
   );
 }
